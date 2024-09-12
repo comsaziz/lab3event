758fbdf3d4aeac73319c2fe67ea91fc1c8d420e7
comsaziz <comsaziz@gmail.com>
2024-09-12 13:37:52 +0300

    add

---
+++ main.js
@@ -0,0 +1,70 @@
+let img1 = document.getElementById('one')
+let img2 = document.getElementById('two')
+let img3 = document.getElementById('three')
+let img4 = document.getElementById('four')
+let img5 = document.getElementById('five')
+let img6 = document.getElementById('six')
+let img7 = document.getElementById('saven')
+let img8 = document.getElementById('eight')
+let img9 = document.getElementById('nine')
+let head = document.getElementById('screen')
+
+
+img1.addEventListener("click", () =>{
+    document.getElementById("tvimg").src ="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/blue-planet.jpg"
+    head.appendChild(img1)
+})
+
+
+img2.addEventListener("click", () =>{
+    document.getElementById("tvimg").src ="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/deadpool.jpg"
+    head.appendChild(img2)
+})
+
+img3.addEventListener("click", () =>{
+    document.getElementById("tvimg").src ="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/gravity.jpg"
+    head.appendChild(img3)
+})
+
+img4.addEventListener("click", () =>{
+    document.getElementById("tvimg").src ="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/good-will-hunting.jpg"
+    head.appendChild(img4)
+})
+
+
+img5.addEventListener("click", () =>{
+    document.getElementById("tvimg").src ="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/cnn.jpg"
+    head.appendChild(img5)
+})
+
+
+img5.addEventListener("click", () =>{
+    document.getElementById("tvimg").src ="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/cnn.jpg"
+    head.appendChild(img5)
+})
+img6.addEventListener("click", () =>{
+    document.getElementById("tvimg").src ="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/koyaanisqatsi.jpg"
+    head.appendChild(img6)
+
+})
+
+img7.addEventListener("click", () =>{
+    document.getElementById("tvimg").src ="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/parts-unknown.jpg"
+    head.appendChild(img7)
+
+})
+img8.addEventListener("click", () =>{
+    document.getElementById("tvimg").src ="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/star-wars-solo.jpeg"
+    head.appendChild(img8)
+
+})
+img9.addEventListener("click", () =>{
+    document.getElementById("tvimg").src ="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/stranger-things.jpg"
+    head.appendChild(img9)
+
+})
+
+
+
+
+
